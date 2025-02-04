a83b47e776c4f5a843f4bb9b33739a922c46092b
shahnoor-oneroot <shahnoor@oneroot.farm>
2025-02-04 13:43:27 +0530

    created redirect to dashboard link

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Page = async () => {
   const user = await onBoardUser();
   if (user?.status == 200 || user.status === 201) {
-    return redirect(`/dashboard/${user.data.firstname}`);
+    return redirect(`/dashboard/${user.data.firstname}${user?.data.lastname}`);
   }
   return <div>{user?.data.firstname}</div>;
 };
