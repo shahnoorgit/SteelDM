615a74b889646847920fb9ea3c359186c9bcfa82
shahnoorgit <shahnoormujawar@gmail.com>
2025-02-25 00:18:33 +0530

    added UI for triggers

---
+++ src/components/global/automation-list/trigger/index.tsx
@@ -0,0 +1,43 @@
+import { InstagramBlue, PlaneBlue } from "@/icons";
+import React from "react";
+
+type Props = {
+  type: string;
+  keywords: {
+    id: string;
+    word: string;
+    automationId: string | null;
+  }[];
+};
+
+const ActiveTrigger = ({ type, keywords }: Props) => {
+  return (
+    <div className=" bg-background-80 rounded-xl p-3 w-full">
+      <div className=" flex gap-x-2 items-center">
+        {type == "COMMENT" ? <InstagramBlue /> : <PlaneBlue />}
+        <p className="text-lg">
+          {type === "COMMENT"
+            ? "User comments on my post"
+            : "User sends me a direct message"}
+        </p>
+      </div>
+      <p className=" text-text-secondary">
+        {type === "COMMENT"
+          ? "If user comments on a video that is setup to listen for keywords, this automation will fire"
+          : "If user send you a message that contains a keyword, this automation will fire"}
+      </p>
+      <div className=" flex gap-2 mt-5 flex-wrap">
+        {keywords?.map((word) => (
+          <div
+            key={word.id}
+            className=" bg-gradient-to-br from-[#3352CC] to-[#1C2D70] flex items-center gap-x-2 capitalize text-white font-light py-1 px-4 rounded-full"
+          >
+            {word.word}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ActiveTrigger;
--- src/components/global/automations/trigger/index.tsx
+++ src/components/global/automations/trigger/index.tsx
@@ -1,11 +1,54 @@
+"use client";
+import { useQueryAutomation } from "@/hooks/user.queries";
 import React from "react";
+import ActiveTrigger from "../../automation-list/trigger";
+import { Separator } from "@/components/ui/separator";
 
 type Props = {
   id: string;
 };
 
 const Trigger = ({ id }: Props) => {
-  return <div>Trigger</div>;
+  const data = useQueryAutomation(id);
+  // if (data?.data && data?.data?.trigger.length > 0) {
+  return (
+    <div className="flex flex-col gap-y-6 items-center">
+      <ActiveTrigger
+        type={"COMMENT"}
+        keywords={[
+          {
+            id: "42121$ghj",
+            word: "LOL",
+            automationId: id,
+          },
+        ]}
+      />
+
+      <>
+        <div className=" relative w-6/12 mt-4">
+          <p className=" absolute transform px-2 -translate-y-1/2 top-1/2 -translate-x-2 left-1/2">
+            or
+          </p>
+          <Separator
+            orientation="horizontal"
+            className=" border-muted border-[1px]"
+          />
+        </div>
+      </>
+
+      <ActiveTrigger
+        type={"MESSAGE"}
+        keywords={[
+          {
+            id: "42121$ghj",
+            word: "DM",
+            automationId: id,
+          },
+        ]}
+      />
+    </div>
+  );
+  // }
 };
 
 export default Trigger;
